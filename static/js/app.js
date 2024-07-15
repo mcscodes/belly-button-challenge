2d01281b59642416ca39a53166869ce5ac989a1a
Matthew Sanders <matthewcsanders@icloud.com>
2024-07-15 18:13:35 -0400

    Updated

--- static/js/app.js
+++ static/js/app.js
@@ -3,20 +3,23 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    var metadata = data.metadata
 
     // Filter the metadata for the object with the desired sample number
-
+    var resultArray = metadata.filter (obj => obj.id == sample);
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    var panel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-
+    resultArray.forEach((obj) => {
+      Object.entries(obj).forEach(([key, value]) => {
+        panel.append("p").text(`${key}: ${value}`);
+      });
   });
 }
 
